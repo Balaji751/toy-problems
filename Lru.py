dcf1a383b17e130008b8cefd736cf10c14f3c7d8
Sri Balaji <55683994+Balaji751@users.noreply.github.com>
2020-04-13 19:30:11 +0530

    Added constructor

--- Lru.py
+++ Lru.py
@@ -1,10 +1,4 @@
 class Lru:
     def __init__(self):
         self.cache=[]
-    
-    def put(self):
-        pass
-    def get(self):
-        pass
-    def get_cache(self):
-        pass
+        
